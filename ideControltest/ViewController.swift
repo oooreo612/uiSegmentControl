e7cfb8a74e64e52c5db6ec849bef1f8ecd5b1aeb
brianke <oooreo612@gmail.com>
2022-10-27 13:12:53 +0800

    Initial Commit

---
+++ ideControltest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ideControltest
+//
+//  Created by 柯昭全 on 2022/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
